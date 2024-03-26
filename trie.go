90b1d7c791ddcd82d0efce90d35205ca5dec6391
_ <pete@p-sys76.localdomain>
2024-03-26 18:57:42 -0400

    tries

---
+++ trie.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	// "fmt"
+	"strings"
+)
+
+type Node struct {
+	isKey    bool
+	children map[rune]*Node
+}
+
+func newNode() *Node {
+	return &Node{
+		isKey:    false,
+		children: make(map[rune]*Node),
+	}
+}
+
+type Trie struct {
+	root *Node
+}
+
+func newTrie() *Trie {
+	trie := Trie{
+		root: newNode(),
+	}
+	return &trie
+}
+
+func (t Trie) Contains(key string) bool {
+	currentNode := t.root
+	searchChars := []rune(key)
+	for len(searchChars) >= 1 {
+		c := searchChars[0]
+		child, hasChild := currentNode.children[c]
+		if hasChild {
+			currentNode = child
+			searchChars = searchChars[1:]
+		} else {
+			return false
+		}
+	}
+	return currentNode.isKey
+}
+
+func (t *Trie) addNewBranch(node *Node, chars []rune) {
+	currentNode := node
+
+	for len(chars) >= 1 {
+		c := chars[0]
+		currentNode.children[c] = newNode()
+		currentNode = currentNode.children[c]
+		chars = chars[1:]
+	}
+	currentNode.isKey = true
+
+}
+func (t *Trie) Insert(s string) bool {
+	currentNode := t.root
+	searchChars := []rune(s)
+	for len(searchChars) >= 1 {
+		c := searchChars[0]
+		child, hasChild := currentNode.children[c]
+		if hasChild {
+			currentNode = child
+			searchChars = searchChars[1:]
+		} else {
+			t.addNewBranch(currentNode, searchChars)
+		}
+	}
+	currentNode.isKey = true
+	return currentNode.isKey
+}
+func (t *Trie) Remove(key string) bool {
+	return false
+}
+func (t *Trie) LongestPrefix(key string) string {
+	return ""
+}
+func (t *Trie) KeysStartingWith(prefix string) []string {
+	return []string{}
+}
+
+type stack[T any] struct {
+	data []T
+}
+
+func (s *stack[T]) push(item T) {
+	s.data = append(s.data, item)
+}
+func (s *stack[T]) pop() T {
+	n := len(s.data) - 1
+	item := s.data[n]
+	s.data = s.data[:n]
+	return item
+}
+func (s *stack[T]) peek() T {
+	n := len(s.data) - 1
+	return s.data[n]
+}
+
+func (s *stack[T]) size() int {
+	return len(s.data)
+}
+
+type NodePath struct {
+	prefix string
+	node   *Node
+}
+
+func enumerate(n *Node) []string {
+	var neighborStack stack[NodePath]
+	var enumeration []string
+	var prefix = ""
+	neighborStack.push(NodePath{prefix, n})
+
+	for neighborStack.size() > 0 {
+		currentPath := neighborStack.pop()
+		if currentPath.node.isKey {
+			// fmt.Println(currentPath.prefix)
+			enumeration = append(enumeration, currentPath.prefix)
+		}
+		for c, neigbor := range currentPath.node.children {
+			neighborStack.push(
+				NodePath{
+					prefix: currentPath.prefix + string(c),
+					node:   neigbor,
+				})
+		}
+	}
+	return enumeration
+}
+func (t *Trie) Enumerate() []string {
+	return enumerate(t.root)
+}
+
+func (t *Trie) String() string {
+	return strings.Join(enumerate(t.root), "\n")
+}
+
+type StringContainer interface {
+	Insert(key string) bool
+	Remove(key string) bool
+	Contains(key string) bool
+	LongestPrefix(key string) string
+	KeysStartingWith(prefix string) []string
+}
---
+++ trie_test.go
@@ -0,0 +1,73 @@
+package main
+
+import "testing"
+
+func TestCreate(t *testing.T) {
+	var sc *Trie = newTrie()
+	if sc == nil {
+		t.Fatalf("StringContainer.New ")
+	}
+}
+func TestInsert(t *testing.T) {
+	strings := []string{"ant", "anthem", "anteater"}
+	var trie *Trie = newTrie()
+	for _, word := range strings {
+		insertResult := trie.Insert(word)
+		if !insertResult {
+			t.Fatalf("\nExpected:\ttrue\nActual:\t%v", insertResult)
+		}
+	}
+
+}
+func TestEnumerate(t *testing.T) {
+	strings := []string{"ant", "anthem", "anteater"}
+	var trie *Trie = newTrie()
+	var insertResult bool = true
+	for _, word := range strings {
+		insertResult = insertResult && trie.Insert(word)
+	}
+	results := trie.Enumerate()
+	expectedNResults := len(strings)
+	actualNResults := len(results)
+	if expectedNResults != actualNResults {
+		t.Fatalf("\nExpected %v results; got %v results", expectedNResults, actualNResults)
+	}
+	t.Logf("Trie:\n%v\n", trie)
+}
+func TestRemove(t *testing.T) {
+
+}
+func TestContains_Positive(t *testing.T) {
+	strings := []string{"ant", "anthem", "anteater"}
+	var trie *Trie = newTrie()
+	var insertResult bool = true
+	for _, word := range strings {
+		insertResult = insertResult && trie.Insert(word)
+	}
+	key := "ant"
+	containsKey := trie.Contains(key)
+	if !containsKey {
+		t.Fatalf("Expected trie to contain %v\n", key)
+	}
+}
+func TestContains_Negative(t *testing.T) {
+	strings := []string{"ant", "anthem", "anteater"}
+	var trie *Trie = newTrie()
+	var insertResult bool = true
+	for _, word := range strings {
+		insertResult = insertResult && trie.Insert(word)
+	}
+	key := "blarg"
+	containsKey := trie.Contains(key)
+	if containsKey {
+		t.Fatalf("Did not expect trie to contain key %v\n", key)
+	}
+}
+
+func TestLongestPrefix(t *testing.T) {
+
+}
+
+func TestKeysWithPrefix(t *testing.T) {
+
+}
